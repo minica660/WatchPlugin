acf1fb461e2f0b363b3847178f8a48ca3458cf63
MiniCash <tminecraft2143@gmail.com>
2026-08-08 22:47:55 +0900

    Add files via upload

---
+++ src/main/java/MiniCash/watch/Watch.java
@@ -0,0 +1,16 @@
+package MiniCash.watch;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Watch extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
